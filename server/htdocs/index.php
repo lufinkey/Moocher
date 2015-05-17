4c4e70a895d24192c77f3dd8d610f1a1d86d580a
lufinkey <luisfinke@gmail.com>
2015-05-17 10:20:58 -0400

    Made a basic front page

--- server/htdocs/index.php
+++ server/htdocs/index.php
@@ -1,5 +1,10 @@
-<?php
-
-echo "hello!";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="css/style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div class="moocher_logo"></div>
+</body>
+</html>
