745a5ee23d2f264505033f9e8aa891d6f5d6b7b2
Luis Finke <luisfinke@gmail.com>
2015-05-16 21:47:12 -0400

    added some dummy data

---
+++ moocher_database_dummydata.sql
@@ -0,0 +1,3 @@
+insert into user values(1,"root","deadbabies","2015-05-16 14:41:49.055309");
+insert into location values(1,1, -84.5155, 39.132,"2015-05-16 14:53:00.000000","2016-05-17 23:53:00.000000","Go to the 8th floor of Baldwin hall");
+insert into tag values(1, "food"), (1, "shelter");
